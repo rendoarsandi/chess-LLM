f2813e6df6b697c2e0edeaf6ac5747ca9afae383
rendo arsandi <rendoarsandi@gmail.com>
2025-12-19 15:20:15 +0700

    feat(server): Start GameLoopService on server startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { db } from './db'
+import { GameManager } from './game/game-manager'
+import { GameService } from './game/game.service'
+import { RandomPlayer } from './game/random-player'
+import { GameLoopService } from './game/game-loop.service'
 
 const app = new Hono()
+
+// Initialize services
+const gameManager = new GameManager()
+const gameService = new GameService(db, gameManager)
+const randomPlayer = new RandomPlayer()
+const gameLoopService = new GameLoopService(db, gameService, randomPlayer)
+
+// Start background loop
+gameLoopService.start(5000)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
